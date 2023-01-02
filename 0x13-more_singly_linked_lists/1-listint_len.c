65f198ff62e0fab8b8c521cac045d2bd6dd7162e
Fawaz Adeniji <fawazadeniji123@gmail.com>
2023-01-02 15:09:35 +0100

    formated scrips according to betty guide lines

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,12 +11,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes = 0;
+size_t nodes = 0;
 
-	while (h)
-	{
-		nodes++;
-		h = h->next;
-	}
-	return (nodes);
+while (h)
+{
+nodes++;
+h = h->next;
+}
+return (nodes);
 }
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,15 +9,15 @@
 
 void free_listint2(listint_t **head)
 {
-  listint_t *current = *head;
-  listint_t *next;
+listint_t *current = *head;
+listint_t *next;
 
-  while (current != NULL)
-  {
-    next = current->next;
-    free(current);
-    current = next;
-  }
+while (current != NULL)
+{
+next = current->next;
+free(current);
+current = next;
+}
 
-  *head = NULL;
+*head = NULL;
 }
