53ff4fac725b81c0b4022aefd2dcc2c8610bbb78
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-09-03 12:00:24 +0100

    refactor: fix indentation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,24 +5,25 @@
  *
  * Return: the converted value
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int decimal = 0;
-int str_len = 0, base = 1;
+	unsigned int decimal = 0;
+	int str_len = 0, base = 1;
 
-if (!check_valid_string(b))
-return (0);
+	if (!check_valid_string(b))
+	return (0);
 
-while (b[str_len] != '\0')
-str_len++;
+	while (b[str_len] != '\0')
+		str_len++;
 
-while (str_len)
-{
-decimal += ((b[str_len - 1] - '0') * base);
-base *= 2;
-str_len--;
-}
-return (decimal);
+	while (str_len)
+	{
+		decimal += ((b[str_len - 1] - '0') * base);
+		base *= 2;
+		str_len--;
+	}
+	return (decimal);
 }
 
 /**
@@ -31,16 +32,17 @@ return (decimal);
  *
  * Return: 1 if string is valid, 0 otherwise
  */
+
 int check_valid_string(const char *b)
 {
-if (b == NULL)
-return (0);
+	if (b == NULL)
+		return (0);
 
-while (*b)
-{
-if (*b != '1' && *b != '0')
-return (0);
-b++;
-}
-return (1);
+	while (*b)
+	{
+		if (*b != '1' && *b != '0')
+			return (0);
+		b++;
+	}
+	return (1);
 }
