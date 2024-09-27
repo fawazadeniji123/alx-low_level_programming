5a27fbee0aed220acc6a349b3b7bd8be3071662b
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-09-27 17:13:08 +0100

    refactor: Fix off-by-one error in jump_search function

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -29,7 +29,7 @@ int jump_search(int *array, size_t size, int value)
 	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
 
 	jump = jump > size - 1 ? jump : size - 1;
-	for (; i < jump && array[i] < value; i++)
+	for (; i < jump - 1 && array[i] < value; i++)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
