46254f6c768b2c9175fac5d4419dfbcd77045195
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-09-27 12:54:54 +0100

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-gcc -Wall -c -fPIC ./*.c 
-gcc  -shared ./*.o  -o liball.so 
+
+# Create a dynamic library from all .c files in the current directory
+
+# Get all .c files in the current directory
+SOURCE_FILES=$(ls *.c)
+
+# Compile the .c files into position-independent object files (.o)
+gcc -c -fPIC $SOURCE_FILES
+
+# Create the dynamic library called liball.so from the object files
+gcc -shared -o liball.so *.o
+
+
